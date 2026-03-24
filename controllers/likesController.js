2a4ba8e57d6823417559fcfe65e8b8cf1ddd2a99
biolonga <josehenriquedasilvadias20@gmail.com>
2026-03-24 04:22:48 -0300

    Add likes controller for handling likes logic

---
+++ controllers/likesController.js
@@ -0,0 +1,19 @@
+// likesController.js
+
+// Function to handle liking a post
+export const likePost = (postId) => {
+    // Logic for liking a post
+    console.log(`Post ${postId} liked.`);
+};
+
+// Function to handle unliking a post
+export const unlikePost = (postId) => {
+    // Logic for unliking a post
+    console.log(`Post ${postId} unliked.`);
+};
+
+// Function to get likes count for a post
+export const getLikesCount = (postId) => {
+    // Logic to get likes count
+    console.log(`Getting likes count for post ${postId}.`);
+};
